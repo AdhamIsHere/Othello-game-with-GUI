104f4548f9ba5f9865808c4f0ee35bfdf3403e47
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-05-14 03:45:48 +0300

    hopefully fixed ai ☝

--- AlphaBetaPruning.py
+++ AlphaBetaPruning.py
@@ -29,6 +29,9 @@
 #                     if beta <= alpha:
 #                         break
 #         return minEval
+from Board import Board
+
+
 class AIPlayer:
     def __init__(self, color):
         self.__color = color
@@ -36,62 +39,70 @@ class AIPlayer:
     def getColor(self):
         return self.__color
 
-    # def utility(self, board):
-    #     return board.countColor(self.__color) - board.countColor(board.playerRev(self.__color))
-    #
-    # def alphaBetaPruning(self, board, depth, alpha=float("-inf"), beta=float("inf"), maximizingPlayer=True):
-    #     if depth == 0 or board.isGameOver():
-    #         return self.utility(board)
-    #
-    #     if maximizingPlayer:
-    #         maxEval = float('-inf')
-    #         for row in range(8):
-    #             for col in range(8):
-    #                 if board.getCell(row, col).getColor() == '':
-    #                     newBoard = board.copy()
-    #                     newBoard.setColor(row, col, self.__color)
-    #                     eval = self.alphaBetaPruning(newBoard, depth - 1, alpha, beta, False)
-    #                     maxEval = max(maxEval, eval)
-    #                     alpha = max(alpha, eval)
-    #                     if beta <= alpha:
-    #                         break
-    #         return maxEval
-    #     else:
-    #         minEval = float('inf')
-    #         for row in range(8):
-    #             for col in range(8):
-    #                 if board.getCell(row, col).getColor() == '':
-    #                     newBoard = board.copy()
-    #                     newBoard.setColor(row, col, self.__color)
-    #                     eval = self.alphaBetaPruning(newBoard, depth - 1, alpha, beta, True)
-    #                     minEval = min(minEval, eval)
-    #                     beta = min(beta, eval)
-    #                     if beta <= alpha:
-    #                         break
-    #         return minEval
-    #
-    # def getBestMove(self, board, depth):
-    #     bestMove = None
-    #     maxEval = float('-inf')
-    #     alpha = float('-inf')
-    #     beta = float('inf')
-    #     for row in range(8):
-    #         for col in range(8):
-    #             if board.getCell(row, col).getColor() == '':
-    #                 newBoard = board
-    #                 newBoard.getCell(row, col).setColor(self.__color)
-    #                 eval = self.alphaBetaPruning(newBoard, depth - 1, alpha, beta, False)
-    #                 if eval > maxEval:
-    #                     maxEval = eval
-    #                     bestMove = (row, col)
-    #     return bestMove
+    def utility(self, board , maximizingPlayer):
+        if maximizingPlayer:
+            return board.countColor('black') - board.countColor('white')
+        else:
+            return board.countColor('white') - board.countColor('black')
 
-    def utility(self, board):
-        return board.countColor(self.__color) - board.countColor(board.playerRev(self.__color))
+
 
     def alphaBetaPruning(self, board, depth, alpha=float("-inf"), beta=float("inf"), maximizingPlayer=True):
         if depth == 0 or board.isGameOver():
             return self.utility(board)
+
+        if maximizingPlayer:
+            maxEval = float('-inf')
+            for row in range(8):
+                for col in range(8):
+                    if board.getCell(row, col).getColor() == '':
+                        newBoard = board.copy()
+                        newBoard.setColor(row, col, self.__color)
+                        eval = self.alphaBetaPruning(newBoard, depth - 1, alpha, beta, False)
+                        maxEval = max(maxEval, eval)
+                        alpha = max(alpha, eval)
+                        if beta <= alpha:
+                            break
+            return maxEval
+        else:
+            minEval = float('inf')
+            for row in range(8):
+                for col in range(8):
+                    if board.getCell(row, col).getColor() == '':
+                        newBoard = board.copy()
+                        newBoard.setColor(row, col, self.__color)
+                        eval = self.alphaBetaPruning(newBoard, depth - 1, alpha, beta, True)
+                        minEval = min(minEval, eval)
+                        beta = min(beta, eval)
+                        if beta <= alpha:
+                            break
+            return minEval
+
+    def getBestMove(self, board, depth):
+        bestMove = None
+        maxEval = float('-inf')
+        alpha = float('-inf')
+        beta = float('inf')
+        for row in range(8):
+            for col in range(8):
+                if board.getCell(row, col).getColor() == '':
+                    newBoard = board
+                    newBoard.getCell(row, col).setColor(self.__color)
+                    eval = self.alphaBetaPruning(newBoard, depth - 1, alpha, beta, False)
+                    if eval > maxEval:
+                        maxEval = eval
+                        bestMove = (row, col)
+        return bestMove
+
+    def utility(self, board , maximizingPlayer):
+        if maximizingPlayer:
+            return board.countColor('black') - board.countColor('white')
+        else:
+            return board.countColor('white') - board.countColor('black')
+
+    def alphaBetaPruning(self, board, depth, alpha=float("-inf"), beta=float("inf"), maximizingPlayer=True):
+        if depth == 0 or board.isGameOver():
+            return self.utility(board, maximizingPlayer)
 
         if maximizingPlayer:
             maxEval = float('-inf')
@@ -138,3 +149,4 @@ class AIPlayer:
 
                     alpha = max(alpha, eval)
         return bestMove
+
